73ae8f9cacb8efcc759982eeea4d0e613e6efd3b
Kilian <fol.kilian@spock-bot.xyz>
2026-01-29 22:12:33 +0100

    Update Topbar: add hover effects, transitions, and improve navigation link styling for enhanced interactivity and layout consistency

--- app/components/General/Topbar.vue
+++ app/components/General/Topbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixed z-100 top-0 left-0 w-full h-16 bg-dark shadow-xl shadow-cyan-50/10 flex items-center justify-around text-white">
     <a
-      class="flex items-center gap-4"
+      class="flex items-center gap-4 hover:rotate-2 hover:scale-110 transition duration-300 ease-in-out"
       href="/public"
     >
       <lazy-nuxt-img
@@ -13,9 +13,25 @@
       <h2><b>Glyria Cloud</b></h2>
     </a>
     <div class="flex items-center gap-4">
-      <a href="">Fonctionnalités</a>
-      <a href="">Tarif</a>
-      <a href="">Formation</a>
+      <NuxtLink
+        to="#terminal"
+        class="relative inline-block after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-primary after:transition-all after:duration-300 hover:after:w-full"
+      >
+        Terminal
+      </NuxtLink>
+      <NuxtLink
+        to="#fonctionnalites"
+        class="relative inline-block after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-primary after:transition-all after:duration-300 hover:after:w-full"
+      >
+        Fonctionnalités
+      </NuxtLink>
+      <NuxtLink
+        to="#pourquoi-nous"
+        class="relative inline-block after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-primary after:transition-all after:duration-300 hover:after:w-full"
+      >
+        Pourquoi nous
+      </NuxtLink>
+
       <UButton
         color="primary"
         variant="solid"
