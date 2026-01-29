7ce3cfc68722bd9d3939d3e649594d69ec9933d0
Kilian <67502590+Kiki344r@users.noreply.github.com>
2026-01-29 17:23:04 +0100

    Updated nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,43 @@ export default defineNuxtConfig({
     }
   },
 
+  tailwindcss: {
+    config: {
+      content: [
+        './app/components/**/*.{js,vue,ts}',
+        './app/layouts/**/*.vue',
+        './app/pages/**/*.vue',
+        './app/plugins/**/*.{js,ts}',
+        './app/app.vue',
+        './app/**/*.{js,vue,ts}',
+        './node_modules/@nuxt/ui/dist/**/*.{js,vue,ts}'
+      ],
+      safelist: [
+        'bg-linear-to-l',
+        'bg-linear-120',
+        'from-primary',
+        'to-secondary',
+        'from-primary/60',
+        'to-primary/50',
+        'from-secondary/60',
+        'to-secondary/50',
+        'from-info/60',
+        'to-info/40',
+        'from-success/60',
+        'to-success/50',
+        'border-primary',
+        'border-secondary',
+        'border-info',
+        'border-success',
+        'text-primary',
+        'text-secondary',
+        'bg-dark',
+        'shadow-cyan-50/10',
+        'grid-background'
+      ]
+    }
+  },
+
   compatibilityDate: '2025-01-15',
 
   vite: {
