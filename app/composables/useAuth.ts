4fef69e4499197ec77b034492f7a3944c01100c4
Kilian <fol.kilian@spock-bot.xyz>
2026-01-31 21:01:33 +0100

    Update useAuth composable: add Login and Register functions with toast-based feedback and API integration

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -1,12 +1,68 @@
 const router = useRouter()
+const { requestPost } = useApi()
+const toast = useToast()
 
-const GithubLogin = () => {
-  console.log('Github login')
-  return router.push('/auth/github')
+const Login = async (data: any) => {
+  const { status, data: res } = await requestPost({
+    version: 1,
+    route: 'auth/login',
+    data: data
+  })
+
+  if (!status || !res) return false
+
+  console.log(res)
+
+  if (res.success) {
+    localStorage.setItem('useData', res.data)
+
+    toast.add({
+      title: 'Connexion réussie',
+      description: `Bienvenue ${res?.data?.firstName} !`,
+      color: 'success'
+    })
+
+    return router.push('/')
+  } else {
+    toast.add({
+      title: 'Erreur',
+      description: res?.message,
+      color: 'error'
+    })
+    return false
+  }
+}
+
+const Register = async (data: any) => {
+  const { status, data: res } = await requestPost({
+    version: 1,
+    route: 'auth/register',
+    data: data
+  })
+
+  if (!status || !res) return false
+
+  if (res.success) {
+    toast.add({
+      title: 'Compte créer avec succès',
+      description: `Vous pouvez vous connecter avec vos identifiants !`,
+      color: 'success'
+    })
+
+    return router.push('/')
+  } else {
+    toast.add({
+      title: 'Erreur',
+      description: res?.message,
+      color: 'error'
+    })
+    return false
+  }
 }
 
 export default function () {
   return {
-    GithubLogin
+    Login,
+    Register
   }
 }
