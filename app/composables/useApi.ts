b4598b368d8802d5ddcf7275ec1fcd3d0f60ec43
Kilian <fol.kilian@spock-bot.xyz>
2026-02-01 18:21:01 +0100

    Refactor `useApi` composable: unify request functions into `requestInternal` for improved reusability and maintainability, and simplify API method handling with cleaner interfaces.

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -1,7 +1,3 @@
-const runtimeConfig = useRuntimeConfig()
-const apiUrl = runtimeConfig.public.api_url
-const toast = useToast()
-
 interface response {
   success: boolean
   message?: string
@@ -20,153 +16,82 @@ interface requestGet {
   options?: Record<string, string>
 }
 
-const requestPost = async (data: request) => {
-  try {
-    const res = await $fetch(`${apiUrl}/v${data.version}/${data.route}`, {
-      method: 'post',
-      credentials: 'include',
-      body: data.data
-    }) as response
-    return {
-      status: true,
-      data: res
-    }
-  } catch (e: any) {
-    if (e?.data?.message) {
-      toast.add({ title: 'Erreur', description: e.data.message, color: 'error' })
+interface requestInternal {
+  version: number
+  route: string
+  method: 'post' | 'delete' | 'put' | 'patch' | 'get'
+  data?: any
+  options?: Record<string, string>
+}
+
+export default function useApi() {
+  const runtimeConfig = useRuntimeConfig()
+  const apiUrl = runtimeConfig.public.api_url
+  const toast = useToast()
+
+  const requestInternal = async (data: requestInternal, enableToast: boolean = true) => {
+    try {
+      let requestUrl = `${apiUrl}/v${data.version}/${data.route}`
+
+      if (data.method === 'get' && data.options) requestUrl += `?${new URLSearchParams(data.options)}`
+
+      const res = await $fetch(`${requestUrl}`, {
+        method: data.method,
+        cache: 'no-store',
+        credentials: 'include',
+        body: data?.data || null
+      }) as response
       return {
-        status: false,
-        message: e.data.message
+        status: true,
+        data: res
       }
-    } else {
-      toast.add({ title: 'Erreur', description: 'Impossible de contacter l\'API de Glyria Cloud.', color: 'error' })
-      return {
-        status: false,
-        message: e
+    } catch (e: any) {
+      if (e?.data?.message) {
+        if (enableToast) toast.add({ title: 'Erreur', description: e.data.message, color: 'error' })
+        return {
+          status: false,
+          message: e.data.message
+        }
+      } else {
+        if (enableToast) toast.add({ title: 'Erreur', description: 'Impossible de contacter l\'API de Glyria Cloud.', color: 'error' })
+        return {
+          status: false,
+          message: e
+        }
       }
     }
   }
-}
 
-const requestDelete = async (data: request) => {
-  try {
-    const res = await $fetch(`${apiUrl}/v${data.version}/${data.route}`, {
-      method: 'delete',
-      credentials: 'include',
-      body: data.data
-    }) as response
-
-    return {
-      status: true,
-      data: res
-    }
-  } catch (e: any) {
-    if (e?.data?.message) {
-      toast.add({ title: 'Erreur', description: e.data.message, color: 'error' })
-      return {
-        status: false,
-        message: e.data.message
-      }
-    } else {
-      toast.add({ title: 'Erreur', description: 'Impossible de contacter l\'API de Glyria Cloud.', color: 'error' })
-      return {
-        status: false,
-        message: e
-      }
-    }
+  const requestPost = async (data: request, enableToast: boolean = true) => {
+    const req = data as requestInternal
+    req.method = 'post'
+    return await requestInternal(req, enableToast)
   }
-}
 
-const requestPut = async (data: request) => {
-  try {
-    const res = await $fetch(`${apiUrl}/v${data.version}/${data.route}`, {
-      method: 'put',
-      credentials: 'include',
-      body: data.data
-    }) as response
-
-    return {
-      status: true,
-      data: res
-    }
-  } catch (e: any) {
-    if (e?.data?.message) {
-      toast.add({ title: 'Erreur', description: e.data.message, color: 'error' })
-      return {
-        status: false,
-        message: e.data.message
-      }
-    } else {
-      toast.add({ title: 'Erreur', description: 'Impossible de contacter l\'API de Glyria Cloud.', color: 'error' })
-      return {
-        status: false,
-        message: e
-      }
-    }
+  const requestDelete = async (data: request, enableToast: boolean = true) => {
+    const req = data as requestInternal
+    req.method = 'delete'
+    return await requestInternal(req, enableToast)
   }
-}
 
-const requestPatch = async (data: request) => {
-  try {
-    const res = await $fetch(`${apiUrl}/v${data.version}/${data.route}`, {
-      method: 'patch',
-      credentials: 'include',
-      body: data.data
-    }) as response
-
-    return {
-      status: true,
-      data: res
-    }
-  } catch (e: any) {
-    if (e?.data?.message) {
-      toast.add({ title: 'Erreur', description: e.data.message, color: 'error' })
-      return {
-        status: false,
-        message: e.data.message
-      }
-    } else {
-      toast.add({ title: 'Erreur', description: 'Impossible de contacter l\'API de Glyria Cloud.', color: 'error' })
-      return {
-        status: false,
-        message: e
-      }
-    }
+  const requestPut = async (data: request, enableToast: boolean = true) => {
+    const req = data as requestInternal
+    req.method = 'put'
+    return await requestInternal(req, enableToast)
   }
-}
 
-const requestGet = async (data: requestGet) => {
-  try {
-    const res = await $fetch(
-      `${apiUrl}/v${data.version}/${data.route}?${new URLSearchParams(data.options)}`,
-      {
-        method: 'get',
-        credentials: 'include'
-      }
-    ) as response
-
-    return {
-      status: true,
-      data: res
-    }
-  } catch (e: any) {
-    if (e?.data?.message) {
-      toast.add({ title: 'Erreur', description: e.data.message, color: 'error' })
-      return {
-        status: false,
-        message: e.data.message
-      }
-    } else {
-      toast.add({ title: 'Erreur', description: 'Impossible de contacter l\'API de Glyria Cloud.', color: 'error' })
-      return {
-        status: false,
-        message: e
-      }
-    }
+  const requestPatch = async (data: request, enableToast: boolean = true) => {
+    const req = data as requestInternal
+    req.method = 'patch'
+    return await requestInternal(req, enableToast)
   }
-}
 
-export default function () {
+  const requestGet = async (data: requestGet, enableToast: boolean = true) => {
+    const req = data as requestInternal
+    req.method = 'get'
+    return await requestInternal(req, enableToast)
+  }
+
   return {
     requestPost,
     requestDelete,
