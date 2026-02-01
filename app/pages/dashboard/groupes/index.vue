053ace6a4efeb09f096e8b604c0f7d4b32c9f657
Kilian <fol.kilian@spock-bot.xyz>
2026-02-01 22:01:53 +0100

    Add `groupes` index page with `UDashboardSidebarToggle` and `DashboardGroupesList` components

---
+++ app/pages/dashboard/groupes/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <UDashboardSidebarToggle
+      variant="subtle"
+      color="primary"
+      class="absolute top-5 left-5 transform -translate-x-1/2 -translate-y-1/2"
+    />
+    <DashboardGroupesList />
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'dashboard'
+})
+</script>
