6879e31bf5e778fa154465f68abd0e545e6d1ab3
Kilian <fol.kilian@spock-bot.xyz>
2026-02-01 22:02:00 +0100

    Add `DashboardGroupesList` component displaying a list of groups with fetched data

---
+++ app/components/Dashboard/Groupes/List.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Liste des groupes</h1>
+    <ul>
+      <li
+        v-for="group in groups"
+        :key="group.UUID"
+      >
+        Nom: {{ group.name }} <br>
+        OwnerId: {{ group.ownerId }}<br>
+        UUID: {{ group.UUID }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { getGroups } = useGroups()
+const groupsReq = await getGroups()
+const groups = ref<{ UUID: string, name: string, ownerId: string }[]>([])
+
+if (groupsReq) {
+  groups.value = groupsReq
+}
+</script>
