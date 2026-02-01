82fb59612ced7388258848255779aeb3b066252b
Kilian <fol.kilian@spock-bot.xyz>
2026-02-01 18:20:48 +0100

    Add dashboard index page with UDashboardSidebarToggle and dashboard layout meta

---
+++ app/pages/dashboard/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <UDashboardSidebarToggle
+      variant="subtle"
+      color="primary"
+      class="absolute top-5 left-5 transform -translate-x-1/2 -translate-y-1/2"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({ layout: 'dashboard' })
+</script>
