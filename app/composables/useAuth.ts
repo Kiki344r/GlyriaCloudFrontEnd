0029dd68372a87e7d0a9a7f5044db3f24ccdbb9e
Kilian <fol.kilian@spock-bot.xyz>
2026-02-01 18:21:06 +0100

    Update `useAuth` composable: refactor structure, add `verifyToken` and `Account` methods, and enhance token verification and caching functionality

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -1,68 +1,107 @@
-const router = useRouter()
-const { requestPost } = useApi()
-const toast = useToast()
+export default function useAuth() {
+  const router = useRouter()
+  const { requestPost, requestGet } = useApi()
+  const toast = useToast()
 
-const Login = async (data: any) => {
-  const { status, data: res } = await requestPost({
-    version: 1,
-    route: 'auth/login',
-    data: data
-  })
+  const verifyToken = async (redirectIfExpired: boolean = false, enableToast: boolean = true) => {
+    const { status, data } = await requestGet({
+      version: 1,
+      route: 'auth/me'
+    }, enableToast)
 
-  if (!status || !res) return false
+    if (!status || !data) return false
 
-  console.log(res)
+    if (!data.success) {
+      toast.add({
+        title: 'Erreur',
+        description: data?.message,
+        color: 'error'
+      })
+      localStorage.removeItem('useData')
+      await cookieStore.delete('token')
+      if (redirectIfExpired) return router.push('/login')
+      return false
+    }
 
-  if (res.success) {
-    localStorage.setItem('useData', res.data)
-
-    toast.add({
-      title: 'Connexion réussie',
-      description: `Bienvenue ${res?.data?.firstName} !`,
-      color: 'success'
-    })
-
-    return router.push('/')
-  } else {
-    toast.add({
-      title: 'Erreur',
-      description: res?.message,
-      color: 'error'
-    })
-    return false
+    localStorage.setItem('useData', JSON.stringify(data.data))
+    return true
   }
-}
 
-const Register = async (data: any) => {
-  const { status, data: res } = await requestPost({
-    version: 1,
-    route: 'auth/register',
-    data: data
-  })
-
-  if (!status || !res) return false
-
-  if (res.success) {
-    toast.add({
-      title: 'Compte créer avec succès',
-      description: `Vous pouvez vous connecter avec vos identifiants !`,
-      color: 'success'
+  const Login = async (data: any) => {
+    const { status, data: res } = await requestPost({
+      version: 1,
+      route: 'auth/login',
+      data
     })
 
-    return router.push('/')
-  } else {
-    toast.add({
-      title: 'Erreur',
-      description: res?.message,
-      color: 'error'
-    })
-    return false
+    if (!status || !res) return false
+
+    if (res.success) {
+      localStorage.setItem('useData', res.data)
+
+      toast.add({
+        title: 'Connexion réussie',
+        description: `Bienvenue ${res?.data?.firstName} !`,
+        color: 'success'
+      })
+
+      return router.push('/')
+    } else {
+      toast.add({
+        title: 'Erreur',
+        description: res?.message,
+        color: 'error'
+      })
+      return false
+    }
   }
-}
 
-export default function () {
+  const Register = async (data: any) => {
+    const { status, data: res } = await requestPost({
+      version: 1,
+      route: 'auth/register',
+      data
+    })
+
+    if (!status || !res) return false
+
+    if (res.success) {
+      toast.add({
+        title: 'Compte créer avec succès',
+        description: `Vous pouvez vous connecter avec vos identifiants !`,
+        color: 'success'
+      })
+
+      return router.push('/')
+    } else {
+      toast.add({
+        title: 'Erreur',
+        description: res?.message,
+        color: 'error'
+      })
+      return false
+    }
+  }
+
+  const Account = () => {
+    return {
+      cache: function () {
+        return localStorage.getItem('useData')
+      },
+      fetch: async () => {
+        await verifyToken()
+        return localStorage.getItem('useData')
+      },
+      isLoggedIn: async () => {
+        return await verifyToken(false, false)
+      }
+    }
+  }
+
   return {
+    verifyToken,
     Login,
-    Register
+    Register,
+    Account
   }
 }
