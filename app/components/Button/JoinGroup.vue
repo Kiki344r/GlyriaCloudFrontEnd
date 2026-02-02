49f7c5f922f8a7a24384013d279546736f2ef41d
Kilian <fol.kilian@spock-bot.xyz>
2026-02-02 21:24:30 +0100

    Add `JoinGroup` button component with group join logic

---
+++ app/components/Button/JoinGroup.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <UButton
+      label="Rejoindre un groupe"
+      color="primary"
+      variant="outline"
+      class="cursor-pointer"
+      :on-click="onClick"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const store = useGroupCodeStore()
+
+const onClick = () => {
+  store.open = true
+}
+</script>
