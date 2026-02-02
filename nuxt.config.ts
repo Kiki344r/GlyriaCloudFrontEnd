eebf2954a6584682eac192c607f49497317a2429
Kilian <fol.kilian@spock-bot.xyz>
2026-02-02 21:26:23 +0100

    Update `nuxt.config.ts`: enable `@nuxt/hints` and `@nuxt/a11y` modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', // ,
-  '@nuxt/eslint', // '@nuxt/hints',
-  // '@nuxt/a11y'
-  '@nuxt/image', '@pinia/nuxt', '@nuxtjs/device'],
+  modules: ['@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@pinia/nuxt',
+    '@nuxtjs/device',
+    '@nuxt/hints',
+    '@nuxt/a11y'
+  ],
   ssr: false,
 
   devtools: {
