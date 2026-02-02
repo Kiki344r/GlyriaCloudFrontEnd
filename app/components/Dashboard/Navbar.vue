d05973ca1b235ddd059e9b8da1dd3de183b4838e
Kilian <fol.kilian@spock-bot.xyz>
2026-02-02 21:24:20 +0100

    Add `DashboardNavbar` component with trailing slot for sidebar collapse

---
+++ app/components/Dashboard/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <UDashboardNavbar>
+    <template #trailing>
+      <UDashboardSidebarCollapse />
+    </template>
+  </UDashboardNavbar>
+</template>
+
+<script setup lang="ts">
+
+</script>
