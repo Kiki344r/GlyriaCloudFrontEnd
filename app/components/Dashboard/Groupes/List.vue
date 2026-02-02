523fef1463a912dfb28b01794c730e30d138d03c
Kilian <fol.kilian@spock-bot.xyz>
2026-02-02 21:24:27 +0100

    Update `DashboardGroupesList` component: enhance layout, add empty state, and improve group display details

--- app/components/Dashboard/Groupes/List.vue
+++ app/components/Dashboard/Groupes/List.vue
@@ -1,25 +1,74 @@
 <template>
-  <div>
-    <h1>Liste des groupes</h1>
-    <ul>
-      <li
+  <div class="flex flex-col gap-5 p-5">
+    <h1 class="text-3xl font-semibold">
+      Liste des groupes
+    </h1>
+
+    <UCard v-if="groups.length === 0">
+      <UEmpty
+        title="Aucun groupe rejoint"
+        description="Pour rejoindre un groupe, cliquez sur le bouton ci-dessous."
+        icon="ic:round-groups"
+        :actions="[
+          {
+            icon: 'mdi-plus',
+            label: 'Rejoindre un groupe',
+            color: 'primary',
+            variant: 'outline',
+            class: 'cursor-pointer',
+            onClick: () => {
+              console.log('Clicked')
+              store.open = true
+            }
+          }
+        ]"
+      />
+    </UCard>
+
+    <div
+      v-else
+      class="rounded-lg overflow-hidden bg-default ring ring-default divide-y divide-default grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-5"
+    >
+      <UCard
         v-for="group in groups"
         :key="group.UUID"
       >
-        Nom: {{ group.name }} <br>
-        OwnerId: {{ group.ownerId }}<br>
-        UUID: {{ group.UUID }}
-      </li>
-    </ul>
+        <div class="flex flex-col gap-2">
+          <h3 class="text-xl font-semibold">
+            {{ group.name }}
+          </h3>
+          <p>
+            Propriétaire: {{ group.owner.firstName + ' ' + group.owner.lastName }}
+          </p>
+          <ModalValidGroupLeave :uuid="group.UUID" />
+        </div>
+      </UCard>
+
+      <UEmpty
+        title="Vous trouvez pas votre groupe ?"
+        description="Pour rejoindre un groupe, cliquez sur le bouton ci-dessous."
+        icon="ic:round-groups"
+        :actions="[
+          {
+            icon: 'mdi-plus',
+            label: 'Rejoindre un groupe',
+            color: 'primary',
+            variant: 'outline',
+            class: 'cursor-pointer',
+            onClick: () => {
+              console.log('Clicked')
+              store.open = true
+            }
+          }
+        ]"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const { getGroups } = useGroups()
-const groupsReq = await getGroups()
-const groups = ref<{ UUID: string, name: string, ownerId: string }[]>([])
+const { getGroupsRef } = useGroups()
+const groups = getGroupsRef()
 
-if (groupsReq) {
-  groups.value = groupsReq
-}
+const store = useGroupCodeStore()
 </script>
